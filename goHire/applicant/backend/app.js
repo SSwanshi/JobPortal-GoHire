c67d1dfd8a6095ea66a3036f9ced2e6986d09823
SSwanshi <adityaspgm@gmail.com>
2026-04-15 03:10:08 +0530

    cors issue fixed

--- goHire/applicant/backend/app.js
+++ goHire/applicant/backend/app.js
@@ -30,7 +30,7 @@ app.set('etag', false);
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'http://localhost:3000'
+  'https://gohire-applicant.onrender.com'
 ];
 
 app.use(cors({
